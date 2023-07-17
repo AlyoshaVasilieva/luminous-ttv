8da4902495f325d4e1c9c1f0b8be63316ea280d9
Malloc Voidstar <1284317+AlyoshaVasilieva@users.noreply.github.com>
2023-07-17 00:36:53 -0700

    Bump deps, Firefox user-agent

--- src/common.rs
+++ src/common.rs
@@ -7,7 +7,7 @@ use rand::Rng;
 // retrieve latest Windows Firefox user-agent from
 // https://www.whatismybrowser.com/guides/the-latest-user-agent/firefox
 pub(crate) const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) \
-Gecko/20100101 Firefox/113.0";
+Gecko/20100101 Firefox/115.0";
 
 pub(crate) fn get_rng() -> impl Rng {
     rand::thread_rng()
